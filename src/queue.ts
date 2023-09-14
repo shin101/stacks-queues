95432deb2622cab8f18886330a21805254a75ddd
shin101 <kendras0127@gmail.com>
2023-09-14 00:32:40 -0400

    queue in TS

--- src/queue.ts
+++ src/queue.ts
@@ -1,7 +1,10 @@
 /** Node: node for a queue. */
 
 class Node {
-  constructor(val) {
+  val: number;
+  next: Node | null;
+
+  constructor(val: number) {
     this.val = val;
     this.next = null;
   }
@@ -11,6 +14,9 @@ class Node {
  *  remove from the front or add to the back. */
 
 class Queue {
+  first: Node | null;
+  last: Node | null;
+  size: number;
   constructor() {
     this.first = null;
     this.last = null;
@@ -19,50 +25,48 @@ class Queue {
 
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
-  enqueue(val) {
-    let node = new Node(val);
-
-    if (!this.first){
+  enqueue(val: number) {
+    const node = new Node(val);
+    if (!this.first) {
       this.first = node;
       this.last = node;
     } else {
-      this.last.next = node;
-      this.last = node;
+      if (this.last) {
+        this.last.next = node;
+        this.last = node;
+      }
     }
-    this.size ++;
+    this.size++;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
-    if(this.length === 0){
-      throw new Error("queue is empty!!")
+    if (this.first) {
+      let print = this.first?.val;
+      if (this.first == this.last) {
+        this.last = null;
+      }
+      this.first = this.first.next;
+      this.size--;
+      return print;
+    } else {
+      throw new Error("error");
     }
-    let temp = this.first;
-    this.first = this.first.next;
-    this.size --;
-    return temp.val;
-
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-    if(this.length === 0){
-      throw new Error("nothing in there")
-    } else {
-      return this.first.val;
-    }
-
+    return this.first?.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-    return this.size === 0 
-
+    return this.size === 0;
   }
 }
 
-module.exports = Queue;
+export default Queue;
